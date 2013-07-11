c3886ed92a94e36e4f0f2767764ddd32305cb64f
ospalh <ospalh@gmail.com>
2013-07-11 10:53:16 +0200

    Semi-automatically remove trailing whitespace.

--- HeisigInfo/addheisiginfo.py
+++ HeisigInfo/addheisiginfo.py
@@ -24,16 +24,16 @@ import codecs
 
 # RTK file path
 this_dir, this_filename = os.path.split(__file__)
-allRTK = os.path.join(this_dir, "RTK.tsv") 
+allRTK = os.path.join(this_dir, "RTK.tsv")
 
 #RTK data goes here
 kanjiIndex = dict()
-                    
+
 
 joinseparator = ','
 
 #Fields used
-#in					
+#in
 expField = 'Expression'
 #out
 kanjiDstField = 'Kanji Keywords'
@@ -45,13 +45,13 @@ def readRTK():
     with open(allRTK, 'r') as f:
         content = f.read().splitlines()
         #sys.stderr.write(content[0] + "\n")
-    f.close() 
+    f.close()
     for line in content:
         fieldhash = dict(zip(('kanji', 'heisigold', 'heisig2010', 'keyword'),
                             line.split('\t')))
         kanjiIndex[fieldhash['kanji'].decode('utf8')] = fieldhash
-    
-    
+
+
 def getHeisigVersion():
 
     #sys.stderr.write("setversion")
@@ -62,8 +62,8 @@ def getHeisigVersion():
     yButton = hDialog.addButton("6th Edition", QMessageBox.AcceptRole)
     nButton = hDialog.addButton("Older Edition",QMessageBox.RejectRole)
     hDialog.setStandardButtons(QMessageBox.Cancel);
-    
-    
+
+
     ret = hDialog.exec_()
     if ret == QMessageBox.AcceptRole:
         version = "2010"
@@ -73,39 +73,39 @@ def getHeisigVersion():
         #sys.stderr.write("reject" + str(ret))
     elif ret == QMessageBox.Cancel:
         return False
-        
+
     return version
 
-    
+
 #get user to select from a list
 def getKeyFromList(titleText, labelText, strings):
-        
+
     d = QInputDialog()
     d.setComboBoxItems(strings)
     d.setWindowTitle(titleText)
     d.setLabelText(labelText)
     d.setOption(QInputDialog.UseListViewForComboBoxItems)
-       
-    
+
+
     if d.exec_()==1:
         return d.textValue()
 
-      
+
 def addHeisigNumbers(nids):
     #get version to use
-    
+
     version = getHeisigVersion()
     if version == False:
         return
-    
-    
+
+
     fields = []
-    
+
     anote=mw.col.getNote(nids[0])
     #get the fields of the first note
     for (i, f) in anote.items():
         fields.append(i)
-    
+
     #get input/output fields from user
     expField = getKeyFromList("Select field to read from", "Read relevant kanji/expression from:", fields)
     if (expField is None):
@@ -113,13 +113,13 @@ def addHeisigNumbers(nids):
     heisigNumDstField = getKeyFromList("Select field to write to", "Write Heisig numbers to:", fields)
     if (heisigNumDstField is None):
         return
-    
-    
-    
+
+
+
     mw.checkpoint("Add Heisig Numbers")
     mw.progress.start()
     readRTK()
-    
+
     #For each seleccted card
     for nid in nids:
         note = mw.col.getNote(nid)
@@ -145,17 +145,17 @@ def addHeisigNumbers(nids):
         #sys.stderr.write("Results:" + note[dst])
         note.flush()
     mw.progress.finish()
-    mw.reset()	
-    
+    mw.reset()
+
 def addMostDifficultHeisigNumber(nids):
-      
+
     fields = []
-    
+
     anote=mw.col.getNote(nids[0])
     #get the fields of the first note
     for (i, f) in anote.items():
         fields.append(i)
-    
+
     #get input/output fields from user
     expField = getKeyFromList("Select field to read from", "Read relevant kanji/expression from:", fields)
     if (expField is None):
@@ -163,9 +163,9 @@ def addMostDifficultHeisigNumber(nids):
     heisigNumDstField = getKeyFromList("Select field to write to", "Write Heisig numbers to:", fields)
     if (heisigNumDstField is None):
         return
-    
-    
-    
+
+
+
     mw.checkpoint("Add Heisig Numbers")
     mw.progress.start()
     readRTK()
@@ -193,7 +193,7 @@ def addMostDifficultHeisigNumber(nids):
         #sys.stderr.write("Results:" + note[dst])
         note.flush()
     mw.progress.finish()
-    mw.reset()	
+    mw.reset()
 
 def lookupKanjiInfo(wordsTxt, key):
     #first get only the kanji
@@ -201,24 +201,24 @@ def lookupKanjiInfo(wordsTxt, key):
     results = []
     for word in words:
         if (word in kanjiIndex):
-            results.append(kanjiIndex[word][key]) 
+            results.append(kanjiIndex[word][key])
         else:
             if (key=='heisigold' or key=='heisig2010'):
                 results.append('0')
             else:
                 results.append('??')
-    return results    
+    return results
 
 def addKanjiKeywords_bulk(nids):
-    
+
     fields = []
-    
+
     anote=mw.col.getNote(nids[0])
 
     #get the fields of the first note
     for (i, f) in anote.items():
         fields.append(i)
-    
+
     #get input/output fields from user
     expField = getKeyFromList("Select field to read from", "Read relevant kanji/expression from:", fields)
     if (expField is None):
@@ -226,22 +226,22 @@ def addKanjiKeywords_bulk(nids):
     kanjiDstField = getKeyFromList("Select field to write to", "Write keywords to:", fields)
     if (heisigNumDstField is None):
         return
-    
-    
+
+
     mw.checkpoint("Add Heisig Keywords")
-    readRTK()	
-    
+    readRTK()
+
     #set version
     version = '2010'
-    
+
     #get output field
     #which numbering system do you want to use?
     mw.progress.start()
     #For each seleccted card
     for nid in nids:
         note = mw.col.getNote(nid)
-        
-        
+
+
         src = None
         if expField in note:
             src = expField
@@ -254,8 +254,8 @@ def addKanjiKeywords_bulk(nids):
         if not dst:
             # no dst field then skip card
             continue
-                    
-        
+
+
         #Add the data to the dst field
         if True == doNote(note, expField, kanjiDstField):
             note.flush()
@@ -263,88 +263,88 @@ def addKanjiKeywords_bulk(nids):
     mw.reset()
 
 
-    
+
 def addKanjiKeywords_onFocusLost(flag, note, fidx):
-            
-    
+
+
     #check if the fields exist
     dst = src = None
     global expField
     global kanjiDstField
-    
+
     for c, name in enumerate(mw.col.models.fieldNames(note.model())):
         if name == expField:
             src = expField
             srcIdx = c
         if name == kanjiDstField:
             dst = kanjiDstField
-            
+
     if not src or not dst:
         return flag
-    
+
     # dst field already filled?
     #if note[dst]:
         #return flag
-        
+
     # event coming from src field?
     if fidx != srcIdx:
         return flag
 
     readRTK()
-    
+
     #do the note and handle the results
     if False == doNote(note, None, None):
         return flag
     return True
-    
-    
-    
+
+
+
 def doNote(note, expF=None , kanjiDField=None):
-    
+
     #If no fields were given then use the defaults
     if expF==None:
         global expressionField
     else:
         expressionField = expF
-        
+
     if kanjiDField==None:
         global kanjiDstField
     else:
         kanjiDstField = kanjiDField
-        
+
     #Strip out any annoying HTML
     srcTxt = stripHTML(note[expField])
-    
-    
+
+
     changed = 0
-    
+
     #Add the data to the dst field
     result = joinseparator.join(lookupKanjiInfo(srcTxt, 'keyword'))
     if result and note[kanjiDstField] != result:
         changed = 1
-        note[kanjiDstField] = result 
-    
-    
-   
-    
+        note[kanjiDstField] = result
+
+
+
+
     if changed == 1:
         return True
     else:
         return False
-    
-    
-    
-    
+
+
+
+
 def setupMenu(browser):
 
     browser.form.menuEdit.addSeparator()
-    
+
     heisigMenu = browser.form.menuEdit.addMenu('Heisig Info')
-    
+
     a = QAction("Add Keywords", browser)
     b = QAction("Add Indices", browser)
     c = QAction("Add highest Index", browser)
-    
+
     heisigMenu.addAction(a)
     heisigMenu.addAction(b)
     heisigMenu.addAction(c)
@@ -352,10 +352,10 @@ def setupMenu(browser):
     browser.connect(a, SIGNAL("triggered()"), lambda e=browser: onKanjiKeywords(e))
     browser.connect(b, SIGNAL("triggered()"), lambda e=browser: onHeisigNumbers(e))
     browser.connect(c, SIGNAL("triggered()"), lambda e=browser: onHardestHeisigNumber(e))
-                    
+
 def onKanjiKeywords(browser):
     addKanjiKeywords_bulk(browser.selectedNotes())
-    
+
 def onHeisigNumbers(browser):
     addHeisigNumbers(browser.selectedNotes())
 
